d8b1cfc26df43f6cd045a56f8ffc7d9315c6e77f
h1romas4 <h1romas4@gmail.com>
2020-03-18 21:40:08 +0900

    support lambda cache

--- script/api/common.js
+++ script/api/common.js
@@ -11,24 +11,41 @@ import * as request from 'request';
 export const site_url = process.env['URL'];
 
 /**
+ * response cache on AWS Lambda
+ */
+let requestCache = {};
+
+/**
  * http request
  *
  * @param url
  */
 export const requestRawJSON = function(url, callback) {
-    request({ url: url, method: "GET"}, function (error, response, body) {
-        if (error) {
-            callback(null, {
-                statusCode: 500
-            });
-        } else {
-            callback(null, {
-                statusCode: 200,
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: body
-            });
-        }
-    });
+    if(url in requestCache) {
+        callback(null, {
+            statusCode: 200,
+            headers: {
+                "Content-Type": "application/json",
+                "X-E2J-Cache": "lambda"
+            },
+            body: requestCache[url]
+        });
+    } else {
+        request({ url: url, method: "GET"}, function (error, response, body) {
+            if (error) {
+                callback(null, {
+                    statusCode: 500
+                });
+            } else {
+                requestCache[url] = body;
+                callback(null, {
+                    statusCode: 200,
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: body
+                });
+            }
+        });
+    }
 }
