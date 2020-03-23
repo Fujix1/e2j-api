d7170523ae2c9fc946d8f0dc228a6e969f13208b
h1romas4 <h1romas4@gmail.com>
2020-03-24 00:41:09 +0900

    add log for lambda test

--- script/api/common.js
+++ script/api/common.js
@@ -40,13 +40,23 @@ export async function getJsonHttp(url) {
     try {
         if(url.match(/^.+\.tar\.gz$/)) {
             // for .tar.gz arcive
+            console.log("@http start" + url);
             const res = await axios.get(url, { responseType: "arraybuffer" });
             const filename = crypto.createHash('sha1').update(url).digest('hex');
             const tar = "/tmp/" + filename + ".tar.gz";
+            console.log("@http end");
+            console.log("@write file start");
             await fsp.writeFile(tar, res.data);
+            console.log("@write file end");
+            console.log("@decomporess start");
             let ret = await decompress(tar, null, { plugins: [ decompressTarGz() ] });
+            console.log("@decomporess end");
+            console.log("@json parse start");
             let whatsnewjson = JSON.parse(ret[0].data.toString('utf-8'));
+            console.log("@json parse end");
+            console.log("@file remove start");
             await fsp.unlink(tar);
+            console.log("@file remove end");
             jsonCache[url] = whatsnewjson;
         } else {
             // for plane json
